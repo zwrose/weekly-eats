23bb84fcbcd52bae37100c551cd446f7ed32e39f
Zach Rose <zwrose@gmail.com>
2025-10-13 08:40:04 -0400

    fixing user approval

--- src/app/user-management/page.tsx
+++ src/app/user-management/page.tsx
@@ -139,7 +139,7 @@ export default function UserManagementPage() {
         },
         body: JSON.stringify({
           userId,
-          approved,
+          isApproved: approved,
         }),
       });
 
